5b3aa42afaf42a3f54ede02f5d6576bad5a30899
Mita-Nur <luvedoll0.0@gmail.com>
2025-05-15 13:59:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Selamat datang di Website Informatika
+st.image("3416d44708630d1c5ec0fc8a5a7b59dd.jpg", width=200)
+st.write("\n")
+st.subheader("Mita Nur Ballza)
+st.write( 
+    mari latihan membuat gambar karakter dari anime  
