8f6d97b2542b0126dfcd5213e200594d295cb976
Mita-Nur <luvedoll0.0@gmail.com>
2025-05-22 12:56:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,11 @@ st.subheader("Mita Nur Ballza")
 st.write( 
     "mari latihan membuat gambar karakter dari anime" 
 )
+
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
